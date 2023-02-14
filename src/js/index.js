7e497c2008e6b49f2c71256fb1255d0969b19bd8
Milly Arkwright <milly.arkwright@gmail.com>
2023-02-14 11:39:15 +0000

    Content added to accordion config file. Basic HTML & Tailwind CSS added to accordion.html file (roughly looking like the Sketch design). Event listener started in js/index.js.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-console.log(" JS is loading ")
+// console.log(" JS is loading ")
+const accordion = document.getElementsByClassName("accordion")
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active")
+    const panel = this.nextElementSibling
+    if (panel.style.maxHeight) {
+      panel.styvle.maxHeight = null
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px"
+    }
+  })
+}
