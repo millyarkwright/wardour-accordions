2bfd0e1daff78cd0b713d37263fe8f5902ea8b3a
Milly Arkwright <milly.arkwright@gmail.com>
2023-02-14 12:21:03 +0000

    styling of when the accordion is open and closed added to the event listener

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,22 @@
 // console.log(" JS is loading ")
+
 const accordion = document.getElementsByClassName("accordion-header")
 
+// Event Listener to display additional content (article)
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function() {
     this.classList.toggle("active")
     const panel = this.nextElementSibling
     if (panel.style.display === "block") {
       panel.style.display = "none"
+      accordion[i].classList.remove("bg-blue","text-white")
+      accordion[i].classList.add("bg-white")
     } else {
       panel.style.display = "block"
-    }
+      accordion[i].classList.remove("bg-white")
+      accordion[i].classList.add("bg-blue", "text-white")
+    }    
   })
 }
+
+// 
