4d4454fa09107fb46d5107a6147aa3c256b6d3ef
Milly Arkwright <milly.arkwright@gmail.com>
2023-02-14 19:00:04 +0000

    Thick blue border added when user navigates the accordion on keyboard. This DEFINITELY works.

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,52 @@
-// const accordionElements = document.getElementsByClassName("accordion");
-const accordionHeaders = document.getElementsByClassName("accordion-header");
-const toggle = document.getElementsByClassName("toggle");
-// const variant = document.getElementsByTagName("section");
-let currentIndex = -1;
-const keys = [37,38,39,40,32,13]; // Left, Up, Right, Down, Space, Enter
-
+const variant = document.getElementsByTagName('section')
+const accordionHeaders = document.getElementsByClassName('accordion-header')
+const accordionElements = document.getElementsByClassName('accordion')
+const toggle = document.getElementsByClassName('toggle')
+let currentIndex = -1
+const keys = [37,38,39,40,32,13] // Left, Up, Right, Down, Space, Enter
 
 // Remove Border
 const removeBorder = (index) => {
-  accordionHeaders[index].classList.remove("border");
-};
+  if (variant[0].classList.contains('bg-white')){
+    console.log('test removeborder')
+    // accordionHeaders[index].classList.remove("border");
+    accordionElements[index].classList.remove('border-blue-darker', 'border-4')
+  } else {
+    accordionElements[index].classList.remove('border-blue-darker', 'border', 'border-4')
+  }
+}
 
 // Add Border
 const addBorder = (index) => {
-  accordionHeaders[index].classList.add("border");
-};
+  if (variant[0].classList.contains('bg-white')){
+    // accordionHeaders[index].classList.add("border");
+    console.log('test addborder')
+    accordionElements[index].classList.add('border-blue-darker', 'border-4')
+
+  } else {
+    accordionElements[index].classList.add( 'border-blue-darker', 'border', 'border-4' )
+  }
+}
 
 
 // Event Listener for Click/Touch
 for (let i = 0; i < accordionHeaders.length; i++) {
-  accordionHeaders[i].addEventListener("click", function() {
-    toggleAccordion(i);
-  });
+  accordionHeaders[i].addEventListener('click', function() {
+    toggleAccordion(i)
+  })
 }
 
 // Event Listener for Keyboard Navigation. 
-document.addEventListener("keydown", function(event) {
+document.addEventListener('keydown', function(event) {
   if (keys.includes(event.keyCode) && currentIndex === -1){
-    currentIndex = 0;
+    currentIndex = 0
     addBorder(currentIndex)
 
     // ! Left or up arrow pressed
   } else if (event.keyCode === 37 || event.keyCode === 38) {
     removeBorder(currentIndex)
     if (currentIndex > 0) {
-      currentIndex--;
+      currentIndex--
       addBorder(currentIndex)
     }
 
@@ -43,36 +55,35 @@ document.addEventListener("keydown", function(event) {
     removeBorder(currentIndex)
 
     if (currentIndex < accordionHeaders.length - 1) {
-      currentIndex++;
+      currentIndex++
       addBorder(currentIndex)
     }
 
     // ! Enter key or space bar pressed
-  } else if (event.key === "Enter" || event.keyCode === 32) {
-    removeBorder(currentIndex)
-    toggleAccordion(currentIndex);
+  } else if (event.key === 'Enter' || event.keyCode === 32) {
+    toggleAccordion(currentIndex)
   }
-});
+})
 
 // Toggle active class, show/hide panel, change toggle text
 
 const toggleAccordion = (index) => {
-  accordionHeaders[index].classList.toggle("active");
-  const panel = accordionHeaders[index].nextElementSibling;
-  if (panel.style.display === "block") {
-    panel.style.display = "none";
-    panel.classList.add("hidden")
+  accordionHeaders[index].classList.toggle('active')
+  const panel = accordionHeaders[index].nextElementSibling
+  if (panel.style.display === 'block') {
+    panel.style.display = 'none'
+    panel.classList.add('hidden')
     // Header Styling
-    accordionHeaders[index].classList.remove("bg-blue", "text-white");
-    accordionHeaders[index].classList.add("bg-white");
-    toggle[index].textContent = "+";
+    accordionHeaders[index].classList.remove('bg-blue', 'text-white')
+    accordionHeaders[index].classList.add('bg-white')
+    toggle[index].textContent = '+'
   } else {
-    panel.style.display = "block";
-    panel.classList.remove("hidden");
+    panel.style.display = 'block'
+    panel.classList.remove('hidden')
     // Header Styling
-    accordionHeaders[index].classList.remove("bg-white");
-    accordionHeaders[index].classList.add("bg-blue", "text-white");
-    toggle[index].textContent = "x";
+    accordionHeaders[index].classList.remove('bg-white')
+    accordionHeaders[index].classList.add('bg-blue', 'text-white')
+    toggle[index].textContent = 'x'
   }
 }
 
