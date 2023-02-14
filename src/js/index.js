d56afebfee5b9b99d60c7dac84d9ae5ebdab0e1a
Milly Arkwright <milly.arkwright@gmail.com>
2023-02-14 12:11:22 +0000

    functionality for showing article/panel added. Amended the toggle button HTML.

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
 // console.log(" JS is loading ")
-const accordion = document.getElementsByClassName("accordion")
+const accordion = document.getElementsByClassName("accordion-header")
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function() {
     this.classList.toggle("active")
     const panel = this.nextElementSibling
-    if (panel.style.maxHeight) {
-      panel.styvle.maxHeight = null
+    if (panel.style.display === "block") {
+      panel.style.display = "none"
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px"
+      panel.style.display = "block"
     }
   })
 }
