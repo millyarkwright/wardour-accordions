09ad5fb3707477a5f8354f117ac5eb71d64d667b
Milly Arkwright <milly.arkwright@gmail.com>
2023-02-14 12:49:22 +0000

    Play around with adding a transition for opening and closing the accordion so it's smoother. Not complete/working. To return to once navigating accordion by keyboard is complete and setting fractal to display all three background colors

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // console.log(" JS is loading ")
 
 const accordion = document.getElementsByClassName("accordion-header")
+const toggle = document.getElementById("toggle")
 
 // Event Listener to display additional content (article)
 for (let i = 0; i < accordion.length; i++) {
@@ -11,12 +12,34 @@ for (let i = 0; i < accordion.length; i++) {
       panel.style.display = "none"
       accordion[i].classList.remove("bg-blue","text-white")
       accordion[i].classList.add("bg-white")
+      toggle.textContent = "+"
     } else {
       panel.style.display = "block"
       accordion[i].classList.remove("bg-white")
       accordion[i].classList.add("bg-blue", "text-white")
+      toggle.textContent = "x"
+
     }    
   })
 }
+
+// for (let i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener("click", function() {
+//     this.classList.toggle("active")
+//     const panel = this.nextElementSibling
+//     if (panel.classList.contains("open")) {
+//       panel.classList.remove("open")
+//       accordion[i].classList.remove("bg-blue","text-white")
+//       accordion[i].classList.add("bg-white")
+//       toggle.textContent = "+"
+//     } else {
+//       panel.classList.add("open")
+//       accordion[i].classList.remove("bg-white")
+//       accordion[i].classList.add("bg-blue", "text-white")
+//       toggle.textContent = "x"
+//     }    
+//   })
+// }
+
 
 // 
